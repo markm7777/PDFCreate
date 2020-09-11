f95d70e753b4a4823e0f8a7462e072adce397f84
markm7777 <mark.murphy7777@gmail.com>
2020-09-11 09:13:35 -0700

    Updates

---
+++ precache-manifest.99c2fc8649eaedbf5287db7aaff97384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0504c692c23ed235be8723f0028baa",
+    "url": "/markm7777/PDFCreate.git/index.html"
+  },
+  {
+    "revision": "6434049ba0371f866319",
+    "url": "/markm7777/PDFCreate.git/static/css/main.42e590ae.chunk.css"
+  },
+  {
+    "revision": "949f428c6565ae1ed272",
+    "url": "/markm7777/PDFCreate.git/static/js/2.fe3756d5.chunk.js"
+  },
+  {
+    "revision": "ada33127c1eb53d091fe173071ab2076",
+    "url": "/markm7777/PDFCreate.git/static/js/2.fe3756d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6434049ba0371f866319",
+    "url": "/markm7777/PDFCreate.git/static/js/main.8d873d77.chunk.js"
+  },
+  {
+    "revision": "46dd2f859d1821e2983c",
+    "url": "/markm7777/PDFCreate.git/static/js/runtime-main.0cc3aaec.js"
+  },
+  {
+    "revision": "64a7b67e2ff50b5ebd8083d12c5867e5",
+    "url": "/markm7777/PDFCreate.git/static/media/ROBERTDEYL_Logo02.64a7b67e.jpg"
+  }
+]);
